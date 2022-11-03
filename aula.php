786c9e51506d9ac2956515e0f1767472e44f4fa2
Ellen Vieira Da Silva <vieiraellen755@gmail.com>
2022-11-03 11:56:20 -0300

    Novas Alterações

---
+++ aula.php
@@ -0,0 +1,33 @@
+<?php
+    $premio = 1547.90;
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 2 - UC13</title>
+</head>
+<body>
+    
+    <table>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Telefone</th>
+        <th>Prêmio</th>
+        <tr>
+            <td>1254</td>
+            <td>Ellen</td>
+            <td>2185-9200</td>
+            <td><?php echo($premio * 0.12); ?></td>
+        </tr>
+        <tr>
+            <td>1298</td>
+            <td>Mayara</td>
+            <td>2185-9231</td>
+            <td><?php echo($premio * 0.28); ?></td>
+        </tr>
+    </table>
+
+</body>
+</html>
